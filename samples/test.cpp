7e5c46ad6374f7eea17b3bb300f8c4cd60255d39
Srikantha2001 <srikanthaml2001@gmail.com>
2022-07-24 17:10:23 +0530

    Final Test cases

--- samples/test.cpp
+++ samples/test.cpp
@@ -1,53 +1,69 @@
-int hello =10;
 
-class Sample{
+// //-----------------------------------------------------------------------------------
 
-public:
-	int a=1,b=2;
-	int add(){
-		int c=10;
-		long d=1e4;
-		float e =d;
-		char f='m';
-		return a+b;}
+// //                          TEST CASE -1
 
-};
+// // -------------------------------------------------------------------------------------
 
-class Test{
+// int hello =10;
 
-public:
+// class Sample{
 
-	Sample s;
-	Sample s2 =s;
-	Sample *s3 = new Sample();
-	Test(){
+// public:
+// 	int a=1,b=2;
+// 	int add(){
+// 		int c=10;
+// 		long d=1e4;
+// 		float e =d;
+// 		char f='m';
+// 		return a+b;}
 
-	}
-	Test(const Test& t){
-		int h=1;	
-	}
-};
+// };
 
-class Test2:public Test{
-	char k ='c';
-};
+// class Test{
 
-int main(){
-	Test t;
+// public:
 
-	Test t2 =t;
-	Test *t3=new Test2() ;
-	Test t4=*t3;
-}
+// 	Sample s;
+//  Sample s2;
+// 	Sample *s3 = new Sample();
+// 	Test(){
 
+// 	}
+// 	Test(const Test& t){
+// 		int h=1;	
+// 	}
+// };
+
+// class Test2:public Test{
+// 	char k ='c';
+// };
+
+// int main(){
+// 	Test t;
+
+// 	Test t2 =t;
+// 	Test *t3=new Test2() ;
+// 	Test t4=*t3;
+// }
+
+
+// //-----------------------------------------------------------------------------------
+
+// //                          TEST CASE -2
+
+// // -------------------------------------------------------------------------------------
 
 // class BigObject{
 // public:
 // 	int a=1;
 // 	int b=2;
 // 	int c=3;
-// 	double d[100];
+// 	int d=4;
+// 	double e[100];
 // };
+
+
 
 
 // class Foo {
@@ -60,9 +76,51 @@ int main(){
 // 	};
 // };
 
+
 // int main(){
 // 	Foo foo;
-// 	BigObject obj = foo.bar(); 
+
+// 	BigObject big_obj = foo.bar(); 
 // }
 
+// //-----------------------------------------------------------------------------------
 
+// //                          TEST CASE - 3
+
+// // -------------------------------------------------------------------------------------
+
+
+// class SmallObject{
+// public:
+// 	int e =10;
+// 	int f = 20;
+// };
+
+// int main(){
+// 	SmallObject small_obj ;
+// 	SmallObject s2=small_obj;
+// }
+
+// //-----------------------------------------------------------------------------------
+
+// //                          TEST CASE - 4
+
+// // -------------------------------------------------------------------------------------
+
+
+class outer_class{
+public:
+	class inner_class{
+		double largeary[100];
+	};
+
+	inner_class* get_inner_class_instance(){
+		return new inner_class();
+	}
+};
+
+int main(){
+	outer_class o ;
+	auto *i = o.get_inner_class_instance();
+	auto i2 = i;
+}
